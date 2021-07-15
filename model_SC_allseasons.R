80bec0c9a5fa2d1244f88741601cc6566f64a3fd
mbozigar <41131764+mbozigar@users.noreply.github.com>
2021-07-15 10:15:44 -0400

    Update model_SC_allseasons.R

--- model_SC_allseasons.R
+++ model_SC_allseasons.R
@@ -1,3 +1,8 @@
+#' AUTHOR: Matt Bozigar (bozigar@bu.edu)
+#' DATE: 7/15/21
+#' PURPOSE: Define model, read-in analytic data, configure and build model, run, and view diagnostics and results
+
+
 rm(list = ls()) # clears the environment
 getwd()
 
